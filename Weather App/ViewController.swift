e8ef78acdd5fa2885a0d652a17c83c6e12655ea7
GJteVelde <gerjan.te.velde@gmail.com>
2019-05-03 11:09:12 +0200

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Gerjan te Velde on 03/05/2019.
+//  Copyright © 2019 Gerjan te Velde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
